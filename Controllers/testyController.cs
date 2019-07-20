44bf5837f569a16438f358d52e6114378eb8f18a
Raymond <github@galcomp.net>
2019-07-20 12:47:41 +0100

    Initial stage01 version

---
+++ Controllers/testyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace TestyMcTestfaceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testyController : ControllerBase
+    {
+
+
+        [HttpPost("handler")]
+        public async Task<SkillResponse> handler([FromBody] SkillRequest skillRequest)
+        {
+            return ResponseBuilder.Tell("Hi, I'm Testy McTestFace");
+        }
+
+
+
+    }
+}
